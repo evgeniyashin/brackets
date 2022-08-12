5d84dbbbec115846f3cbda85c52a70159dd99ddc
evgeniyashin <106561672+evgeniyashin@users.noreply.github.com>
2022-08-12 22:13:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
 module.exports = function check(str, bracketsConfig) {
-   let brackets = bracketsConfig.join('').replace(/,/g, '');
-  //console.log(brackets);
-  
-  for (i=0; i<brackets.length; i++) {
-    let subStr = `${brackets[i]}${brackets[i+1]}`;
-    if (str.includes(subStr)) {
-      //console.log(subStr);
-      str = str.replace(subStr, '');
-      //console.log(str);
-      i= -1
-      }
-    }
    
-  let result = str.length ? false : true
-  return result
 }
